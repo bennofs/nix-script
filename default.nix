569d336c7f3540fad6f38f8a3248f443717a8226
rnhmjoj <micheleguerinirocco@me.com>
2015-09-11 06:19:17 +0200

    Add new dependency

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ pkgs.stdenv.mkDerivation {
   phases = [ "buildPhase" "installPhase" "fixupPhase" ];
   buildPhase = ''mkdir -p $out/bin; ghc -O2 $src -o $out/bin/nix-script -odir $TMP'';
   installPhase = ''ln -s $out/bin/nix-script $out/bin/nix-scripti'';
-  buildInputs = [ pkgs.haskellPackages.ghc ];
+  buildInputs = [ (pkgs.haskellPackages.ghcWithPackages (hs: with hs; [posix-escape])) ];
   meta = {
     homepage = https://github.com/bennofs/nix-script;
     description = "A shebang for running inside nix-shell.";
